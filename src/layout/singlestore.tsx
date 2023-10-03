701dc2c75ee0af99f24362450fcb8eab06593f21
Shakhmurad <muradb356@gmail.com>
2023-10-03 22:32:03 +0400

    add test

--- src/layout/singlestore.tsx
+++ src/layout/singlestore.tsx
@@ -1,69 +1,147 @@
-import { useParams } from 'react-router'
-import {Container, Stack, Typography } from "@mui/material"
-import axios from "axios"
-import { useEffect, useState } from "react"
-import LayoutContainer from '../companents/layoutContainer'
-const url = 'https://book-db-shakhmurad.vercel.app/posts-card2/'
-function Singlestore() {
-    const [count , setcount] = useState(0)
-    const {id} = useParams()
-    const [data , setdata] = useState([])
-    useEffect(()=>{
-        axios.get(url+id).then(({data})=>{
-            setdata(data)
-        })
-    } , [])
-  return (
-    <>
-    <LayoutContainer>
-        <Container sx={{ py:10,}}>
-            <Stack sx={{justifyContent:'center' , alignItems:'center'}}  >
+// import { useParams } from 'react-router'
+// import {Container, Stack, Typography } from "@mui/material"
+// import axios from "axios"
+// import { useEffect, useState } from "react"
+// import LayoutContainer from '../companents/layoutContainer'
+// const url = 'https://book-db-shakhmurad.vercel.app/posts-card2/'
+// function Singlestore() {
+//     interface IData {
+//         img: string;
+//         item: string;
+//         price: string;
+//         description: string;
+//         publisher: string;
+//         // Add other data fields here
+//     }
+//     const [count , setcount] = useState(0)
+//     const {id} = useParams()
+//     const [data , setdata] = useState<IData>({})
+//     useEffect(()=>{
+//         axios.get(url+id).then(({data})=>{
+//             setdata(data)
+//             console.log(data);
+            
+//         })
+//     } , [])
+//   return (
+//     <>
+//     <LayoutContainer>
+//         <Container sx={{ py:10,}}>
+//             <Stack sx={{justifyContent:'center' , alignItems:'center'}}  >
 
-            <Stack flexDirection={'row'} gap={5} width={{md:'900px' , xs:'100%'}}>
-                <img src={`../Imge/${data.img}.png`}/>
-                <Stack gap={2}>
-                    <Typography sx={{color:"#1B3764" , fontWeight:'700', fontSize:'28px'}} >{data.item}</Typography>
-                    <Typography sx={{color:'#FFCA42' , fontWeight:'700' , fontSize:"22px"}}>#{data.price}USD</Typography>
-                    <Typography sx={{opacity:'0.7'}} >Making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum.</Typography>
+//             <Stack flexDirection={'row'} gap={5} width={{md:'900px' , xs:'100%'}}>
+//                 <img src={`../Imge/${data.img}.png`}/>
+//                 <Stack gap={2}>
+//                     <Typography sx={{color:"#1B3764" , fontWeight:'700', fontSize:'28px'}} >{data.item}</Typography>
+//                     <Typography sx={{color:'#FFCA42' , fontWeight:'700' , fontSize:"22px"}}>$   {data.price}USD</Typography>
+//                     <Typography sx={{opacity:'0.7'}} >Making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum.</Typography>
 
-                    <Stack flexDirection={'row'} gap={2}>
-                        <Typography sx={{opacity:'0.7'}} >Publisher</Typography>
-                        <Typography sx={{opacity:'0.7'}} >Learning Private Limited (1 January 2021)</Typography>
-                    </Stack>
+//                     <Stack flexDirection={'row'} gap={2}>
+//                         <Typography sx={{opacity:'0.7'}} >Publisher</Typography>
+//                         <Typography sx={{opacity:'0.7'}} >Learning Private Limited (1 January 2021)</Typography>
+//                     </Stack>
                     
-                    <Stack flexDirection={'row'} gap={2}>
-                        <Typography sx={{opacity:'0.7'}} >Language :</Typography>
-                        <Typography sx={{opacity:'0.7'}} > English</Typography>
+//                     <Stack flexDirection={'row'} gap={2}>
+//                         <Typography sx={{opacity:'0.7'}} >Language :</Typography>
+//                         <Typography sx={{opacity:'0.7'}} > English</Typography>
+//                     </Stack>
+
+//                     <Stack flexDirection={'row'} gap={2}>
+//                         <Typography sx={{opacity:'0.7'}} >Paperback:</Typography>
+//                         <Typography sx={{opacity:'0.7'}} >212 pages</Typography>
+//                     </Stack>
+
+//                     <Stack flexDirection={'row'} gap={2}>
+//                         <Typography sx={{opacity:'0.7'}} >ISBN-10:</Typography>
+//                         <Typography sx={{opacity:'0.7'}} >9788120345799</Typography>
+//                     </Stack>
+
+
+//                     <Stack flexDirection={'row'} gap={2}>
+//                         <Typography sx={{opacity:'0.7'}} >Dimensions : </Typography>
+//                         <Typography sx={{opacity:'0.7'}} >20 x 14 x 4 cm</Typography>
+//                     </Stack>
+//                     <Stack flexDirection={'row'} gap={1}>
+//                         <p style={{border:'1px solid #FFCA42' , padding:'8px 20px' , cursor:'pointer' , userSelect:'none'}} onClick={()=>setcount(count+1)} >{count}</p>
+//                         <Typography sx={{px:4 , py:1 , backgroundColor:'#FFCA42' , cursor:'pointer' , alignSelf:'center'}} >Add to Cart</Typography>
+//                     </Stack>
+
+//                 </Stack>
+//             </Stack>
+//         </Stack>
+//         </Container>
+
+//     </LayoutContainer>
+//     </>
+//   )
+// }
+
+// export default Singlestore
+
+import { useParams } from 'react-router-dom'; // Import from 'react-router-dom'
+import { Container, Stack, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import LayoutContainer from '../companents/layoutContainer'
+
+const url = 'https://book-db-shakhmurad.vercel.app/posts-card2/';
+
+function Singlestore() {
+    interface IData {
+        img: string;
+        item: string;
+        price: string; // Change to number
+        description: string;
+        publisher: string;
+        // Add other data fields here
+    }
+    
+    const [count, setCount] = useState<number>(0);
+    const { id } = useParams<{ id: string }>();
+    const [data, setData] = useState<IData>({});
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        axios.get(url + id)
+            .then(({ data }) => {
+                setData(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+                setLoading(false);
+            });
+    }, [id]);
+
+    return (
+        <LayoutContainer>
+            <Container sx={{ py: 10 }}>
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <Stack sx={{ justifyContent: 'center', alignItems: 'center' }}>
+                        <Stack flexDirection={'row'} gap={5} width={{ md: '900px', xs: '100%' }}>
+                            <img src={`../Imge/${data.img}.png`} alt={data.item} />
+                            <Stack gap={2}>
+                                <Typography sx={{ color: "#1B3764", fontWeight: '700', fontSize: '28px' }}>{data.item}</Typography>
+                                <Typography sx={{ color: '#FFCA42', fontWeight: '700', fontSize: "22px" }}>${data.price} USD</Typography>
+                                <Typography sx={{ opacity: '0.7' }}>{data.description}</Typography>
+                                <Stack flexDirection={'row'} gap={2}>
+                                    <Typography sx={{ opacity: '0.7' }}>Publisher</Typography>
+                                    <Typography sx={{ opacity: '0.7' }}>{data.publisher} (1 January 2021)</Typography>
+                                </Stack>
+                                {/* Add other data fields here */}
+                                <Stack flexDirection={'row'} gap={1}>
+                                    <p style={{ border: '1px solid #FFCA42', padding: '8px 20px', cursor: 'pointer', userSelect: 'none' }} onClick={() => setCount(count + 1)}>{count}</p>
+                                    <Typography sx={{ px: 4, py: 1, backgroundColor: '#FFCA42', cursor: 'pointer', alignSelf: 'center' }}>Add to Cart</Typography>
+                                </Stack>
+                            </Stack>
+                        </Stack>
                     </Stack>
-
-                    <Stack flexDirection={'row'} gap={2}>
-                        <Typography sx={{opacity:'0.7'}} >Paperback:</Typography>
-                        <Typography sx={{opacity:'0.7'}} >212 pages</Typography>
-                    </Stack>
-
-                    <Stack flexDirection={'row'} gap={2}>
-                        <Typography sx={{opacity:'0.7'}} >ISBN-10:</Typography>
-                        <Typography sx={{opacity:'0.7'}} >9788120345799</Typography>
-                    </Stack>
-
-
-                    <Stack flexDirection={'row'} gap={2}>
-                        <Typography sx={{opacity:'0.7'}} >Dimensions : </Typography>
-                        <Typography sx={{opacity:'0.7'}} >20 x 14 x 4 cm</Typography>
-                    </Stack>
-                    <Stack flexDirection={'row'} gap={1}>
-                        <p style={{border:'1px solid #FFCA42' , padding:'8px 20px' , cursor:'pointer' , userSelect:'none'}} onClick={()=>setcount(count+1)} >{count}</p>
-                        <Typography sx={{px:4 , py:1 , backgroundColor:'#FFCA42' , cursor:'pointer' , alignSelf:'center'}} >Add to Cart</Typography>
-                    </Stack>
-
-                </Stack>
-            </Stack>
-        </Stack>
-        </Container>
-
-    </LayoutContainer>
-    </>
-  )
+                )}
+            </Container>
+        </LayoutContainer>
+    );
 }
 
-export default Singlestore
+export default Singlestore;
