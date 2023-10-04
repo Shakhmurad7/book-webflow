d1df12a1ea234c4a33193df845ae593fc6979cbc
Shakhmurad <muradb356@gmail.com>
2023-10-05 02:17:36 +0400

    add  item in singleCard

--- src/layout/singleCard.tsx
+++ src/layout/singleCard.tsx
@@ -45,6 +45,66 @@ function SingleCard() {
                     <Typography color={'#1B3764'} fontWeight={"700"} variant="h5" component={'h5'}>October 6, 2021 / Classics</Typography>
                     <Typography sx={{opacity:'0.7'}} >There are many variations of passages of Lorem Ipsum available, but the majorithave suffered alteration in some form, by injected humour, or randomised words whicdon't look even slightly believable. If you are going to use a passage of Lorem Ipsum, need to be sure there isn't anything embarrassing hidden in the middle of text. All thLorem Ipsum generators on the Internet tend.</Typography>
                 </Stack>
+                <Stack gap={2} >
+                    <Typography color={'#1B3764'} fontWeight={'700'} variant="h4" component={'h4'}>
+                    The perfect book for all
+                    </Typography>
+                    <Typography sx={{opacity:'0.7'}}>
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.
+                    </Typography>
+
+                    <Stack gap={2} pl={5}>
+                        <Typography position={"relative"} >
+                         It has roots in a piece of classical Latin literature from 45 BC,
+                        <span
+                      style={{
+                        position: "absolute",
+                        content: "",
+                        top: "6px",
+                        left: "-25px",
+                        width: "12px",
+                        height: "12px",
+                        backgroundColor: "#FFCA42",
+                        borderRadius: "50%",
+                      }}
+                    ></span>
+                        </Typography  >
+                        <Typography position={"relative"}>
+                        To generate Lorem Ipsum which looks reasonable
+                        <span
+                      style={{
+                        position: "absolute",
+                        content: "",
+                        top: "6px",
+                        left: "-25px",
+                        width: "12px",
+                        height: "12px",
+                        backgroundColor: "#FFCA42",
+                        borderRadius: "50%",
+                      }}
+                    ></span>
+                        </Typography>
+                        <Typography position={"relative"} >
+                        The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet
+                        <span
+                      style={{
+                        position: "absolute",
+                        content: "",
+                        top: "6px",
+                        left: "-25px",
+                        width: "12px",
+                        height: "12px",
+                        backgroundColor: "#FFCA42",
+                        borderRadius: "50%",
+                      }}
+                    ></span>
+                        </Typography>
+                    </Stack>
+                    <Stack sx={{backgroundColor:'#1B3764', p:6 , color:'white' , textAlign:'center' , position:'relative'}} >
+                        <Typography>“Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.”</Typography>
+                        <img  style={{position:"absolute" , left:'20px' , top:'0px'}} src={'../Imge/Icon (5).svg'}/>
+                    </Stack>
+                </Stack>
             </Container>
         </LayoutContainer>
         </>
