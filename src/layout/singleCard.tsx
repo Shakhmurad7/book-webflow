7f72f1a5cb1302ba70106218b39a456facb5d71c
Shakhmurad <muradb356@gmail.com>
2023-10-05 02:24:16 +0400

    add singleCard Full

--- src/layout/singleCard.tsx
+++ src/layout/singleCard.tsx
@@ -45,13 +45,13 @@ function SingleCard() {
                     <Typography color={'#1B3764'} fontWeight={"700"} variant="h5" component={'h5'}>October 6, 2021 / Classics</Typography>
                     <Typography sx={{opacity:'0.7'}} >There are many variations of passages of Lorem Ipsum available, but the majorithave suffered alteration in some form, by injected humour, or randomised words whicdon't look even slightly believable. If you are going to use a passage of Lorem Ipsum, need to be sure there isn't anything embarrassing hidden in the middle of text. All thLorem Ipsum generators on the Internet tend.</Typography>
                 </Stack>
-                <Stack gap={2} >
+                 <Stack gap={2} >
                     <Typography color={'#1B3764'} fontWeight={'700'} variant="h4" component={'h4'}>
                     The perfect book for all
                     </Typography>
                     <Typography sx={{opacity:'0.7'}}>
                     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.
-                    </Typography>
+                    </Typography>    
 
                     <Stack gap={2} pl={5}>
                         <Typography position={"relative"} >
@@ -104,6 +104,19 @@ function SingleCard() {
                         <Typography>“Crowdsource term sheet freemium beta stealth responsive web design niche market rock star channels.”</Typography>
                         <img  style={{position:"absolute" , left:'20px' , top:'0px'}} src={'../Imge/Icon (5).svg'}/>
                     </Stack>
+                </Stack>
+
+                <Stack gap={2} py={7} >
+                    <Typography color={'#1B3764'} fontWeight={'700'} variant="h4" component={'h4'}>
+                    The perfect book for all
+                    </Typography>
+                    <Typography sx={{opacity:'0.7'}}>
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has less normal distribution of letters, as opposed.
+                    </Typography>   
+                        <Typography sx={{color:'#1B3764' , pl:'30px'}} >1 . It has roots in a piece of classical Latin literature from 45 BC,</Typography>
+                        <Typography sx={{color:'#1B3764' , pl:'30px'}} >2 . To generate Lorem Ipsum which looks reasonable</Typography>
+                        <Typography sx={{color:'#1B3764' , pl:'30px'}} >3 . The first line of Lorem Ipsum, "Lorem ipsum” dolor sit amet</Typography>
+                        <Typography sx={{color:'#1B3764' , pl:'30px'}} >4 . Be standard chunk of Lorem used since the 1500s</Typography>
                 </Stack>
             </Container>
         </LayoutContainer>
